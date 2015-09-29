4eeb0b64ecd4bae9ee6ba7acc66091eae5a50d36
Brian Groat <Barnum7@users.noreply.github.com>
2015-09-29 12:45:24 -0400

    Created Active Record Migrations, 2 seed contacts, and wrote the layout for view/index

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,22 @@
 get '/' do
   erb :index
 end
+
+get'/contacts' do
+	Contact.all.to_json
+end
+
+post '/contacts/create' do
+	response = Hash.new
+	response [:result] = false
+	contact= Contact.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone: params[:phone_number])
+
+	if contact.save
+		response[:result] = true
+		response[:id] = contact.id 
+
+	end
+
+	response.to_json
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Contact.create first_name: "Clark", last_name: "Teeple", email: "clark@email.com", phone_number: "647-227-4321"
+Contact.create first_name: "Mike", last_name: "Tomko", email: "mike@email.com", phone_number: "647-227-1234"
